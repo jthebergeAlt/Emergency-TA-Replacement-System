782286f3d61bebd8dc2d53e829285c0242f81d49
Unknown <jtheberge@outlook.com>
2017-10-29 16:05:59 -0700

    Add flatpickr. Work on express server. Login auth progress. Add style.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const compression = require('compression');
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,8 +9,21 @@ app.use(compression());
 app.use(express.static(__dirname + '/src'));
 app.use(express.static(__dirname));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'src/index.html'))
+  res.sendFile(path.resolve(__dirname, 'src/login.html'))
+});
+
+app.post('/', function (req, res) {
+  if (req.body.password === 'test')
+    res.sendFile(path.resolve(__dirname, 'src/scheduling.html'));
+  else
+    res.sendFile(path.resolve(__dirname, 'src/loginFailed.html'));
 });
 
 app.listen(port);
